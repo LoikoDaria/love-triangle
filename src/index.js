84d400526c23f58ec8e87be4d4ba9cc0b4d54edb
LoikoDaria <36641426+LoikoDaria@users.noreply.github.com>
2018-02-22 23:34:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+var LoveTrianglesCount, s1, s2, s3;
+  LoveTrianglesCount = 0;
+  for(var i=0;i<preferences.length;i++){
+	  s1 = preferences[i]-1;
+	  s2 = preferences[s1]-1;
+	  s3 = preferences[s2]-1;
+	if ((s3 == i) && (s1 != s2) && (s2 != s3) && (s1 != s3)){
+		LoveTrianglesCount++;
+	}
+  }
+  if (LoveTrianglesCount > 0) LoveTrianglesCount = LoveTrianglesCount / 3; 
+  return LoveTrianglesCount;
 };
+
